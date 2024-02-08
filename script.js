ddcd7f73c5b3b02c6602316864a21e655ca95ab2
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-02-08 15:00:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function draw() {
 
 function drawcurve()
 {
-  
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(0,28);
+    ctx.bezierCurveTo(0,28,31,7,70,8);
+    ctx.bezierCurveTo(109,9,313,99,425,90);
+    ctx.bezierCurveTo(516,82.5,526,55,526,55);
 }
 
 draw();
